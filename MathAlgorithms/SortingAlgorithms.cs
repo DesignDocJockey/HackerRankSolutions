cfb189060c96581ce5d2108343b86f4af19bfcde
hew0n0 <hwong18@gmail.com>
2018-03-11 23:21:05 -0400

    Added BubbleSort Implementation

--- MathAlgorithms/SortingAlgorithms.cs
+++ MathAlgorithms/SortingAlgorithms.cs
@@ -6,5 +6,30 @@ namespace MathAlgorithms
 {
     public class SortingAlgorithms
     {
+        public IEnumerable<int> BubbleSort(int[] array)
+        {
+            bool swapOccurred = false;
+            do
+            {
+                swapOccurred = false;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i+1);
+                        swapOccurred = true;
+                    }
+                }
+            } while (swapOccurred);
+
+            return array;
+        }
+
+        private static void Swap(int[] array, int swapToIndex, int swapFromIndex)
+        {
+            var temp = array[swapToIndex];
+            array[swapToIndex] = array[swapFromIndex];
+            array[swapFromIndex + 1] = temp;
+        }
     }
 }
